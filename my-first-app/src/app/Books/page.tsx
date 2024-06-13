2ea225e016afb34b6f6822ecbc590e907bc879f9
NayanPatidar <nayanpatidar28@gmail.com>
2024-06-13 19:33:58 +0530

    Link Added

---
+++ my-first-app/src/app/Books/page.tsx
@@ -0,0 +1,3 @@
+export default function PageTwo() {
+  return <h1>This is Books Page 2</h1>;
+}
--- my-first-app/src/app/page.tsx
+++ my-first-app/src/app/page.tsx
@@ -1,5 +1,12 @@
+import Link from "next/link";
+
 const Home = () => {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <h1>Hello World</h1>
+      <Link href={`/Books`}>Link On Link to Navigate to Books</Link>
+    </>
+  );
 };
 
 export default Home;
