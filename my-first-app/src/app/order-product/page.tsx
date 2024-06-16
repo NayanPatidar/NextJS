7dd1831590e5c18e2e7b14aee86ead6dcdbb2dd3
NayanPatidar <nayanpatidar28@gmail.com>
2024-06-16 23:57:23 +0530

    Navigating

---
+++ my-first-app/src/app/order-product/page.tsx
@@ -0,0 +1,17 @@
+"use client"
+import { useRouter } from "next/navigation";
+
+export default function ProductOrder() {
+  const router = useRouter();
+  const handleClick = () => {
+    console.log("Placing your order");
+    router.push("/");
+  };
+
+  return (
+    <>
+      <h1>Order Product</h1>
+      <button onClick={handleClick}>Place Order</button>
+    </>
+  );
+}
